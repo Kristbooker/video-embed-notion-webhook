3300b5a974fdb372e3b39fb5345b8fab3f39f2ae
Krist Booker <kristsiri.p@gmail.com>
2026-09-05 01:52:14 +0700

    edit main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello Notion!"}
 
 @app.post("/webhook/notion")
 async def notion_webhook(request: Request):
